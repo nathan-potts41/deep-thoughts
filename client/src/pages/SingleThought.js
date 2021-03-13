65d8b5d1beb31a02b08efb42607435ee73e956cb
nathan-potts41 <pottsandpans41@hotmail.com>
2021-03-13 13:55:02 -0600

    added reaction

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import ReactionList from '../components/ReactionList';
+import Auth from '../utils/auth';
+import ReactionForm from '../components/ReactionForm';
 
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHT } from '../utils/queries';
@@ -33,6 +35,7 @@ const SingleThought = props => {
       </div>
 
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
